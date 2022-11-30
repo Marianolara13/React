a2b00162772553512470dcd21eb9b6a06512af7a
Marianolara13 <71089355+Marianolara13@users.noreply.github.com>
2022-11-30 14:10:27 -0600

    Add files via upload

---
+++ part2/2.15-2.16/App.js
@@ -0,0 +1,82 @@
+import React, { useEffect, useState } from 'react'
+import Alert from './components/Alert'
+import Filter from './components/Filter'
+import PersonForm from './components/PersonForm'
+import Persons from './components/Persons'
+import personService from './services/persons'
+
+const App = () => {
+  const [ persons, setPersons ] = useState([]) 
+  const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
+
+  const [ filter, setFilter ] = useState('')
+
+  const hook = () => {
+    personService
+      .getAll()
+      .then(initialPersons => {
+        setPersons(initialPersons)
+      })
+  }
+
+  useEffect(hook, [])
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    const personObject = {
+      name: newName,
+      number: newNumber,
+    }
+
+    if (persons.find(person => 
+      person.name.toLowerCase() === personObject.name.toLowerCase())) {
+        Alert(personObject)
+    } else {
+      personService
+        .create(personObject)
+        .then(returnedPerson => {
+          setPersons(persons.concat(returnedPerson))
+          setNewName('')
+          setNewNumber('')
+        })
+    }
+  }
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
+  const handleFilter = (event) => {
+    setFilter(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <Filter
+        filter={filter}
+        handleFilter={handleFilter}
+      />
+      <h3>Add a new</h3>
+      <PersonForm
+        addPerson={addPerson}
+        newName={newName}
+        handleNameChange={handleNameChange}
+        newNumber={newNumber}
+        handleNumberChange={handleNumberChange}
+      />
+      <h3>Numbers</h3>
+      <Persons
+        persons={persons}
+        filterValue={filter}
+      />
+    </div>
+  )
+}
+
+export default App
